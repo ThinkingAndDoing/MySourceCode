4d65dfc61f06944570ffa3db8c5ece78b4272001
will-xie <xiewei23703@163.com>
2017-04-24 21:46:00 +0800

    更新Rename

--- File system/Rename.py
+++ File system/Rename.py
@@ -18,21 +18,28 @@ def renameAll(length):
 	videolist = []
 	list = os.listdir(".")
 	for item in list:
-		if item.find("mp4")!=-1:
+		ext = item.split(".")
+		if ext[1]!="bat" and ext[1]!="py":
 			videolist.append(item)
 
 	indexA = 1
 	indexB = 1
+	indexS = 1
 	for item in videolist:
 		if item.find("A")!=-1:
-			formatStr = "A"+getFormatStr(length, indexA)+"%d.mp4"
+			formatStr = "A"+getFormatStr(length, indexA)+"%d."+item.split(".")[1]
 			os.rename(item, formatStr%indexA)
 			indexA += 1
 	
 		if item.find("B")!=-1:
-			formatStr = "B"+getFormatStr(length, indexB)+"%d.mp4"
+			formatStr = "B"+getFormatStr(length, indexB)+"%d."+item.split(".")[1]
 			os.rename(item, formatStr%indexB)
 			indexB += 1
+
+		if item.find("S")!=-1:
+			formatStr = "S"+getFormatStr(length, indexS)+"%d."+item.split(".")[1]
+			os.rename(item, formatStr%indexS)
+			indexS += 1
 
 renameAll(4)
 renameAll(3)
