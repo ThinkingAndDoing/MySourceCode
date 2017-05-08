bdb8a9da345ef5c9b589d58c06cd64a6e0984ed5
ThinkingAndDoing <gloomywind321@gmail.com>
2017-05-08 17:37:43 +0800

    排序

---
+++ sort.py
@@ -0,0 +1,23 @@
+#coding=utf-8
+
+def quickSort(L, low, high):
+    i = low 
+    j = high
+    if i >= j:
+        return L
+    key = L[i]
+    while i < j:
+        while i < j and L[j] >= key:
+            j = j-1                                                             
+        L[i] = L[j]
+        while i < j and L[i] <= key:
+            i = i+1 
+        L[j] = L[i]
+    L[i] = key 
+    quickSort(L, low, i-1)
+    quickSort(L, j+1, high)
+    return L
+
+list = ["hello","wei","xie","abc","def"]
+print(list)
+print(quickSort(list, 0, len(list)-1))
