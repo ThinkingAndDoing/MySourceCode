1b989fcdb83f29e319e10f0d62d8334e663cf259
will-xie <xiewei23703@163.com>
2017-05-22 21:45:34 +0800

    Python中调用Javascript

---
+++ Python中调用JavaScript/test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+import execjs
+
+
+def getJS(jsfile):
+	try:
+		f = open(jsfile, "r")
+		js = f.read()
+		ctx = execjs.compile(js)
+	except:
+		print("Error! Exception in getJS!")
+		return None
+	else:
+		return ctx
+
+jser = getJS("tj.js")
+print(jser.call("add", 1, 2))
+print(jser.call("sub", 10, 2))
