92bba8dd040395625cb06165c1390422636c1aea
will-xie <xiewei23703@163.com>
2017-06-29 22:11:40 +0800

    更新文档

---
+++ 网络爬虫/Readme.txt
@@ -0,0 +1,4 @@
+
+
+1，HTTP状态码详解
+http://tool.oschina.net/commons?type=5
--- 网络爬虫/模拟登陆/login_common.py
+++ 网络爬虫/模拟登陆/login_common.py
@@ -7,24 +7,39 @@ http://www.cnblogs.com/nbkhic/p/4216149.html
 button link 跳转
 link = browser.find('a')[0]
 browser.follow_link(link)
+打印对象
+print (object .__dict__)
+print (dir(object))
 '''
 
 import re
+import requests
 from robobrowser import RoboBrowser
 
-url = 'http://d.teamdoc.cn/TPAMemberLogin.aspx'
-b = RoboBrowser(history=True, parser='html.parser')
-b.open(url)
-# 获取登陆表单
-login_form = b.get_form(action='TPAMemberLogin.aspx')
-login_form = b.get_forms()[0]
-# 输入用户名和密码
-login_form['CNameLogin'].value = 'username'
-login_form['CPassword'].value = 'password'
-# 提交表单
-b.submit_form(login_form)
-# 打印登陆成功的信息
-f = open('output.txt','w',encoding='utf-8')
-f.write(str(b.find('html')))
-f.close()
+def login(url, username, password):
+	#cookie={'sc_device_auth':'abcdef'}
+	session = requests.Session()
+	user_agent = 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:47.0) Gecko/20100101 Firefox/47.0'
+ 
+	browser = RoboBrowser(history=True, session=session, user_agent=user_agent, parser='html.parser')
+	browser.open(url, method='post')
+	form = browser.get_form(action='TPAMemberLogin.aspx')
+	#print (form.__dict__)
+
+	form['CNameLogin'] = username
+	form['CPassword'] = password
+	browser.session.headers.update({
+		'Referer': url,
+		'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+		'Accept-Language': 'en-US,en;q=0.5'
+	})
+	#browser.session.cookies.update(cookie)
+	browser.submit_form(form)
+	#print(browser.session.cookies.__dict__)
+	f = open('output.txt','w',encoding='utf-8')
+	f.write(str(browser.find('html')))
+	f.close()
+
+login('http://d.teamdoc.cn/TPAMemberLogin.aspx', 'username', 'password')
+
 
