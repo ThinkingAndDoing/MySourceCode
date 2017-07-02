76b2e8dd1e2f92adee2dad7a4700f1eb3972495c
will-xie <xiewei23703@163.com>
2017-07-02 17:11:39 +0800

    快速排序的扩展

--- 排序/sort.py
+++ 排序/sort.py
@@ -1,4 +1,25 @@
-#coding=utf-8
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+
+def quickSortByIndex (L, low, high, index):
+    i = low 
+    j = high
+    if i >= j:
+        return L
+    key = L[i][index]
+    temp = L[i]
+    while i < j:
+        while i < j and L[j][index] >= key:
+            j = j-1                                                             
+        L[i] = L[j]
+        while i < j and L[i][index] <= key:
+            i = i+1 
+        L[j] = L[i]
+    L[i] = temp 
+    quickSortByIndex (L, low, i-1, index)
+    quickSortByIndex (L, j+1, high, index)
+    return L
 
 def quickSort(L, low, high):
     i = low 
@@ -18,6 +39,16 @@ def quickSort(L, low, high):
     quickSort(L, j+1, high)
     return L
 
-list = ["hello","wei","xie","abc","def"]
-print(list)
-print(quickSort(list, 0, len(list)-1))
+if __name__ == "__main__":
+	listA = []
+	listA.append(["hello",3,"wei"])
+	listA.append(["hello",1,"jun"])
+	listA.append(["hello",2,"qing"])
+	print(listA)
+	print(quickSortByIndex( listA, 0, len(listA)-1, 1))
+
+	listB = ["hello","wei","xie","abc","def"]
+	print(listB)
+	print(quickSort(listB, 0, len(listB)-1))
+	
+	
