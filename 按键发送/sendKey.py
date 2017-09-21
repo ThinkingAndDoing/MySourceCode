6abc495058540334a867ce9220319d8ef878dd7e
will-xie <xiewei23703@163.com>
2017-09-21 20:44:37 +0800

    按键发送

---
+++ 按键发送/sendKey.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+import win32api
+import win32con
+
+if __name__=="__main__":
+	win32api.keybd_event(17,0,0,0)  #ctrl键位码是17
+	win32api.keybd_event(86,0,0,0)  #v键位码是86
+	win32api.keybd_event(86,0,win32con.KEYEVENTF_KEYUP,0) #释放按键
+	win32api.keybd_event(17,0,win32con.KEYEVENTF_KEYUP,0)
+
+
