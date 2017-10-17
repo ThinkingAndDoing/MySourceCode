0a926aa68b44cb24c955a364d2c1d29931fc68c2
ThinkingAndDoing <gloomywind321@gmail.com>
2017-10-17 14:14:55 +0800

    Add files via upload

---
+++ modifyTimeAndAccessOfAllFiles.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import os
+import sys
+import time
+import stat
+
+global _DistPath
+
+_DistPath = "tests"
+
+def modifyTimeAndAccessOfAllFiles(distpath):
+	for root, dirs, files in os.walk(distpath, topdown=False):
+		for name in files + dirs:
+			print(os.path.join(root, name))
+			os.chmod(os.path.join(root, name), stat.S_IWRITE)
+			os.utime(os.path.join(root, name), None)
+
+if __name__=="__main__":
+
+	modifyTimeAndAccessOfAllFiles(_DistPath)
