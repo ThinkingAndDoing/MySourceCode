5f6b9fb3480d57a754760b249143e0f321236cb4
will.xie <610980236@qq.com>
2018-04-14 21:42:35 +0800

    知识管理 改进

--- 读写Json/知识管理/knowledgeOrg.py
+++ 读写Json/知识管理/knowledgeOrg.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
 
+import re
 import os
 import json
 import chardet
+import tkinter
+from tkinter import *
+from tkinter import ttk
+from tkinter.scrolledtext import ScrolledText
 #每个KEY加一个关键词
 #在不同Key之间计算相关度，内容含有相同关键词的，相似度高--那和百度知道有何区别？我想做的是一个知识体系，一个知识体系应该用因果联系串联
 #打印出知识网络
@@ -11,9 +16,12 @@ import chardet
 #http://www.sohu.com/a/140258152_367117
 #https://zhidao.baidu.com/question/167778108.html
 #http://gaozhongwuli.com/zongjie/
-
-_InputDir = ".\\addNewKey"
+#https://blog.csdn.net/bnanoou/article/details/38434443
+#https://www.cnblogs.com/wwf828/p/7418181.html
+_InputDir = ".\\HMI Warning开发"
 _DictFile = ".\\data\\theDict.json"
+_RootKey = "HMI Warning开发"
+_ScrollText = None
 
 _Dict = {}
 
@@ -35,42 +43,40 @@ class Node:
 	def setDecision(self, data):
 		self.decision = data
 
+#--------------------------------------------------
+#存储
+#--------------------------------------------------
+def restoreSrc():
+	global _Dict
+	
+	for key in _Dict.keys():
+		content = ""
+		content +="@how\n"
+		content +=removeEmptyLines(_Dict[key]["how"])+ "\n"
+		content +="@what\n"
+		content +=removeEmptyLines(_Dict[key]["what"])+ "\n"
+		content +="@decision\n"
+		content +=removeEmptyLines(_Dict[key]["decision"])+ "\n"
+		content +="@"
+		saveToLocal(_InputDir+"\\"+_Dict[key]["parent"]+"-"+_Dict[key]["name"]+".txt", content)
+
 def getcharencode(filename):
-	file = open(filename, "rb")#要有"rb"，如果没有这个的话，默认使用gbk读文件。          
+	file = open(filename, "rb")          
 	buf = file.read()
 	file.close()
 	result = chardet.detect(buf)
 	return result["encoding"]
 	
-def getType(data, type):
-	if data.find(type)!=-1:
-		indexstart = data.find(type) + len(type)
-		indexend = data.find("@", indexstart) - 1
-		return data[indexstart:indexend]
-
 def loadKey(fn):
 	f = open(_InputDir+"\\"+fn, "r", encoding=getcharencode(_InputDir+"\\"+fn))
 	data = f.read()
 	f.close()
 	
-	newkey = Node(fn.split("-")[0], fn.split("-")[1].split(".")[0])
+	newkey = Node(fn.split("-")[0].strip(), fn.split("-")[1].split(".")[0].strip())
 	newkey.setWhat(getType(data, "@what"))
 	newkey.setHow(getType(data, "@how"))
 	newkey.setDecision(getType(data, "@decision"))
 	return newkey
-	
-def addKey(key):
-	if key in _Dict:
-		_Dict[key.parent].childs.append(key.name)
-		
-	dictkey = {}
-	dictkey["parent"] = key.parent
-	dictkey["name"] = key.name
-	dictkey["childs"] = key.childs
-	dictkey["what"] = key.what
-	dictkey["how"] = key.how
-	dictkey["decision"] = key.decision
-	_Dict[key.name] = dictkey
 	
 def writeJson(fn, js):
 	with open(fn, 'w') as f:
@@ -86,15 +92,7 @@ def initDict():
 	
 	if os.path.exists(_DictFile):
 		_Dict = readJson(_DictFile)
-	
-def addNewKeys(foldername):
-	global _Dict
-	
-	filelist = os.listdir(foldername)
-	for fn in filelist:
-		newkey = loadKey(fn)
-		addKey(newkey)
-
+		
 def saveToLocal(fn, data):
 	f = open(fn, "w")
 	f.write(data)
@@ -105,14 +103,106 @@ def genAttribs():
 	
 	content = ""
 	for key in _Dict.keys():
-		content +="["+key+"]\n"
-		content +="什么是" + key + "？\n"
+		content +="["+_Dict[key]["parent"]+"->"+key+"]\n"
+		content +="\n什么是" + key + "？\n"
 		content +=_Dict[key]["what"]
-		content +="如何" + key + "？\n"
+		content +="\n如何" + key + "？\n"
 		content +=_Dict[key]["how"]
 		content +="\n"
 	
-	saveToLocal("外汇领域.txt", content)
+	saveToLocal("领域知识.txt", content)
+
+#--------------------------------------------------
+#数据处理，转换
+#--------------------------------------------------
+def removeEmptyLines(data):
+	
+	lines = data.split("\n")
+	newdata = ""
+	for line in lines:
+		if line.strip()!="":
+			newdata += line+"\n"
+	return newdata
+
+def getType(data, type):
+	if data.find(type)!=-1:
+		indexstart = data.find(type) + len(type)
+		indexend = data.find("@", indexstart) - 1
+		return removeEmptyLines(data[indexstart:indexend])
+
+def addRelationship():
+	global _Dict
+	
+	for key in _Dict.keys():
+		if _Dict[key]["parent"] in list(_Dict.keys()):
+			if _Dict[key]["name"] not in _Dict[_Dict[key]["parent"]]["childs"]:
+				_Dict[_Dict[key]["parent"]]["childs"].append(_Dict[key]["name"])
+
+def addKey(key):
+	global _Dict
+
+	dictkey = {}
+	dictkey["parent"] = key.parent
+	dictkey["name"] = key.name
+	dictkey["childs"] = key.childs
+	dictkey["what"] = key.what
+	dictkey["how"] = key.how
+	dictkey["decision"] = key.decision
+	_Dict[key.name] = dictkey
+	
+def addNewKeys(foldername):
+	global _Dict
+	
+	filelist = os.listdir(foldername)
+	for fn in filelist:
+		newkey = loadKey(fn)
+		addKey(newkey)
+	addRelationship()
+
+#--------------------------------------------------
+#UI 显示部分
+#--------------------------------------------------
+def drawGUI():
+	global _ScrollText
+	
+	root=tkinter.Tk()
+	root.title("领域知识")
+	root.geometry("800x480")
+	root.resizable(width=False, height=False)
+	#part one
+	_ScrollText = ScrolledText(root, x=240, y=0, width=80, height=480, background='#f0f0f0')
+	_ScrollText.pack(side=RIGHT, fill=Y)
+	#part two
+	tree=ttk.Treeview(root)
+	vbar = ttk.Scrollbar(root,orient=tkinter.VERTICAL,command=tree.yview)
+	vbar.place(x=200,width=20,height=480)
+	hbar = ttk.Scrollbar(root,orient=tkinter.HORIZONTAL,command=tree.yview)
+	hbar.place(y=460,width=200,height=20)
+	myid=tree.insert("", 0, _Dict[_RootKey]["name"], text=_Dict[_RootKey]["name"])
+	createTree(tree, _Dict[_RootKey]["childs"], myid)
+	tree.configure(yscrollcommand=vbar.set)
+	tree.configure(yscrollcommand=hbar.set)
+	tree.bind("<<TreeviewSelect>>", treeCB)
+	tree.pack(side=LEFT, fill=Y)
+
+	
+	root.mainloop()
+
+def treeCB(event):
+	#event.widget获取Treeview对象，调用selection获取选择对象名称
+	sels= event.widget.selection()
+	_ScrollText.delete(1.0, END)
+	_ScrollText.insert(INSERT, "name:"+_Dict[sels[0]]["name"] + "\n")
+	_ScrollText.insert(INSERT, "how:"+_Dict[sels[0]]["how"] + "\n")
+	_ScrollText.insert(INSERT, "what:"+_Dict[sels[0]]["what"] + "\n")
+	_ScrollText.insert(INSERT, "decision:"+_Dict[sels[0]]["decision"] + "\n")
+	print(sels[0])
+
+def createTree(tree, childList, parentID):
+	print(childList)
+	for child in childList:
+		childID=tree.insert(parentID, childList.index(child), _Dict[child]["name"], text=_Dict[child]["name"])
+		createTree(tree, _Dict[child]["childs"], childID)
 	
 if __name__ == "__main__":
 	initDict()
@@ -122,6 +212,7 @@ if __name__ == "__main__":
 	writeJson(_DictFile, _Dict)
 	
 	genAttribs()
-	
-	
+	#restoreSrc()
+	drawGUI()
+
 	
