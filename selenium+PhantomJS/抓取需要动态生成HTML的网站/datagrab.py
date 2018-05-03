1121b3f6e88cb0c67308fd0c0d662af441e22d6b
will.xie <610980236@qq.com>
2018-05-03 08:20:32 +0800

    selenium phantomJS 抓取动态HTML 更新

--- selenium+PhantomJS/抓取需要动态生成HTML的网站/datagrab.py
+++ selenium+PhantomJS/抓取需要动态生成HTML的网站/datagrab.py
@@ -13,6 +13,17 @@ _username = 'yb'
 _pwd = 'yb'
 _homepage = 'http://218.91.209.251:8087/Default.aspx'
 
+_datatype = ["监测点分钟数据", "监测点小时数据", "监测点日数据", "监测点日报列表", "区域/组小时数据", "区域/组日数据", "区域/组日报列表"]
+
+_datatypedict = {}
+_datatypedict["监测点分钟数据"] = "javascript:__doPostBack('ctl00$ContentHolder$dataNameList$BulletedList1','0')"
+_datatypedict["监测点小时数据"] = "javascript:__doPostBack('ctl00$ContentHolder$dataNameList$BulletedList1','1')"
+_datatypedict["监测点日数据"] = "javascript:__doPostBack('ctl00$ContentHolder$dataNameList$BulletedList1','2')"
+_datatypedict["监测点日报列表"] = "javascript:__doPostBack('ctl00$ContentHolder$dataNameList$BulletedList1','3')"
+_datatypedict["区域/组小时数据"] = "javascript:__doPostBack('ctl00$ContentHolder$dataNameList$BulletedList1','4')"
+_datatypedict["区域/组日数据"] = "javascript:__doPostBack('ctl00$ContentHolder$dataNameList$BulletedList1','5')"
+_datatypedict["区域/组日报列表"] = "javascript:__doPostBack('ctl00$ContentHolder$dataNameList$BulletedList1','6')"
+
 def saveToLocal(fn, str):
 	f = open(fn,'w',encoding='utf-8')
 	f.write(str)
@@ -20,7 +31,6 @@ def saveToLocal(fn, str):
 	
 def waitfortheElement(browser, str):
 	WebDriverWait(browser, 10).until(lambda the_driver: the_driver.find_element_by_link_text(str).is_displayed())
-	#WebDriverWait(ff, 10).until(EC.presence_of_element_located((By.ID, "myDynamicElement")))
 
 def initDriver():
 	chromedriver = "C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe"
@@ -48,28 +58,40 @@ def grab():
 	
 	#step3
 	actions = ActionChains(browser)
-	# 先定位到下拉菜单
-	drop_down = browser.find_element_by_xpath("//div[@id='ContentHolder_dataNameList_Panel1']/table/tbody/tr/td[@class='combo-ddb-cell']")
-	actions.move_to_element(drop_down).click().perform()
-	# 再对下拉菜单中的选项进行选择
-	browser.find_element_by_link_text(u"监测点分钟数据").click() 
 	
+	for dt in _datatype:
+		print(dt)
+		# 先定位到下拉菜单
+		browser.find_element_by_xpath("//div[@id='ContentHolder_dataNameList_Panel1']/table/tbody/tr/td[@class='combo-ddb-cell']").click()
+		# 再对下拉菜单中的选项进行选择
+		#browser.find_element_by_link_text(dt).click()
+		browser.execute_script("comboBoxItemClick(this, '"+dt+"', '"+dt+"')")
+		browser.execute_script(_datatypedict[dt])
+		#点击更新按钮
+		time.sleep(0.2)
+		#WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.ID, "btnUpdateX")))
+		#browser.find_element_by_id('btnUpdateX').click()
+		browser.execute_script("javascript:WebForm_DoPostBackWithOptions(new WebForm_PostBackOptions('ctl00$ContentHolder$btnUpdateX', '', true, '', '', false, true))")
+		time.sleep(0.2)
+		#--------------------------------------------------------------------------
+		#gridview是动态生成的HTML Tag，需要等待此元素可见，才能保存page_source
+		#WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, "gridview")))
+		html_source = browser.page_source
+		
+		soup = BeautifulSoup(html_source, "html.parser")
+		#saveToLocal("output.txt", html_source)
+		saveToLocal(dt.replace("/", "_")+".txt", html_source)
 	
-	WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.ID, "btnUpdateX")))
-	browser.find_element_by_id('btnUpdateX').click()
-	time.sleep(0.2)
-	
-	#gridview是动态生成的HTML Tag，需要等待此元素可见，才能保存page_source
-	WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, "gridview")))
-	html_source = browser.page_source
 	browser.quit()
-
-	soup = BeautifulSoup(html_source, "html.parser")
-	saveToLocal("output.txt", html_source)
-	#--------------------------------------------------------------------------
-	#print(soup.h1.string)
 	
 if __name__ == "__main__":
 	grab()
 
 
+	
+	
+	
+'''
+drop_down = browser.find_element_by_xpath("//div[@id='ContentHolder_dataNameList_Panel1']/table/tbody/tr/td[@class='combo-ddb-cell']")
+actions.move_to_element(drop_down).click().perform()
+'''
